50de7396db4f91858c487f18c17e4bbff1be8636
hangcao1004 <2199950738@qq.com>
2020-06-12 19:40:01 +0800

    add:创建文件

--- FileManageSystem/disk.py
+++ FileManageSystem/disk.py
@@ -1,24 +1,26 @@
-@file
+@file_func('wb')
 def create(fp):
     sp = SuperBlock()
     pwd_inode_id = sp.get_free_inode_id(fp)
     pwd_inode = INode(pwd_inode_id, 10)
     block_id = sp.get_data_block_id(fp)
     pwd_inode._i_sectors[0] = block_id
-    #print("pwd:",block_id)
+    print("pwd:",block_id)
     new_inode_id = sp.get_free_inode_id(fp)
     new_inode = INode(new_inode_id,10)
     filename = "hello"
-    pwd_inode_catlog = CatalogBlock()
-    pwd_inode_catlog.block[new_inode_id] = filename
+    pwd_inode_catlog = CatalogBlock("home")
+    pwd_inode_catlog.son_files[filename] = new_inode_id
     for item in split_serializer(bytes(pwd_inode_catlog)):#把字典写入
         fp.seek(pwd_inode._i_sectors[0] * BLOCK_SIZE)
         fp.write(item)
     #写文件
     s = "world" * (2 ** 8)
-    for i in range(len(serializer(s))):
+    b_text = pickle.dumps(s)
+    block_num = int(ceil(len(b_text) / BLOCK_SIZE))
+    for i in range(block_num):
         new_inode._i_sectors[i] = sp.get_data_block_id(fp)
-        #print("new:",new_inode._i_sectors[i])
+        print("new:",new_inode._i_sectors[i])
 
     k = 0
     for item in serializer(s):
