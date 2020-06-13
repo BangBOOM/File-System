4b9741ff664c6ae9eb63a8b9b7d60135e901d31a
Wentw <wentworthy@outlook.com>
2020-06-13 16:03:29 +0800

    init: 写了一个简易的终端用于测试指令，在running_pfs中

--- FileManageSystem/file_system.py
+++ FileManageSystem/file_system.py
@@ -23,28 +23,59 @@ class FileSystem:
         self.pwd_inode = self.base_inode
         self.path = ['/']  # 用于存储当前路径，每个文件名是一个item
 
+    def get_current_path_name(self):
+        return self.path[-1]
+
+    def pwd(self):
+        return '/'.join(self.path)
+
     def load_disk(self):
+        """
+        加载磁盘，获取超级块
+        :return:
+        """
         self.fp.seek(0)
         return SuperBlock.form_bytes((form_serializer(self.fp, SUPER_BLOCK_NUM)))  # 加载超级块
 
     def get_base_inode(self):
+        """
+        获取根目录的inode
+        :return: INode
+        """
         base_inode_id = self.sp.base_dir_inode_id  # 读取超级块中保存的base节点的块号
         self.fp.seek((INODE_BLOCK_START_ID + base_inode_id) * BLOCK_SIZE)
         return INode.form_bytes(self.fp.read())  # 根据块号加载根目录的inode
 
     def load_pwd_obj(self):
+        """
+        获取当前inode对应的对象
+        :return: string or CatalogBlock
+        """
         return self.pwd_inode.get_target_obj(self.fp)
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        上下文管理，推出的时候写回超级块，根节点inode，当前节点inode
+        :param exc_type:
+        :param exc_val:
+        :param exc_tb:
+        :return:
+        """
         self.sp.write_back(self.fp)
         self.base_inode.write_back(self.fp)
         self.pwd_inode.write_back(self.fp)
 
 
 def file_system_func(func):
+    """
+    文件系统的装饰器包装上下文管理器，简化实际编写的时候的代码不需要使用with直接在函数上加个装饰器即可
+    :param func:
+    :return:
+    """
+
     def func_wrapper():
         with FilePointer('ab+') as fp:
             with FileSystem(fp) as fs:
--- FileManageSystem/running_pfs.py
+++ FileManageSystem/running_pfs.py
@@ -3,16 +3,31 @@ author:Wenquan Yang
 time:2020/6/12 22:50
 intro:文件系统实际运行部分
 """
+from file_system import FileSystem
 from file_system import file_system_func
 
 
 @file_system_func
 def running_pfs_for_test(fs):
-    print(fs.sp)
     pwd_obj = fs.load_pwd_obj()
     return pwd_obj.name
 
 
 @file_system_func
-def running_pfs(fs):
-    pass
+def running_pfs(fs:FileSystem):
+    print("Welcome to the PFS")
+    while True:
+        print("root@pfs:{}".format(fs.get_current_path_name()))
+        cmd=input("> ")
+        if cmd == "pwd":
+            print(fs.pwd())
+        elif cmd == "exit":
+            break
+
+def main():
+    running_pfs()
+
+if __name__ == '__main__':
+    main()
+
+
